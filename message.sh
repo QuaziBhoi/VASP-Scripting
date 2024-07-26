ac921226827c89c77d146c6a0e8df4c31687d2dd
QuaziBhoi <quazibhai543@gmail.com>
2024-07-26 22:52:44 +0600

    Update message.sh

--- message.sh
+++ message.sh
@@ -5,3 +5,14 @@
 	MESSAGE=hello
 	
   curl 'https://api.telegram.org/bot$TOKEN/sendMessage?chat_id=$CHAT_ID&text=$MESSAGE'
+
+#### Send Telegram Message ####
+curl "https://api.telegram.org/bot6605743120:AAHsqkBpVtDt9vKtJRU7bcsSPLq7RLY3nx0/sendMessage?chat_id=6698838969&text={Finished Static Calculation}"
+
+cp ~/bash/dosplot.py ./
+python dosplot.py
+
+curl -s -X POST "https://api.telegram.org/bot6605743120:AAHsqkBpVtDt9vKtJRU7bcsSPLq7RLY3nx0/sendDocument" \
+-F "chat_id=6698838969" \
+-F "document=@./DOS_Plot.png" \
+-F "caption=DOS Plot"
