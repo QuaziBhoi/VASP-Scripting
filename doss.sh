4dbf87f4c4e43bd79a7323a1d541191f0bdcbbb2
QuaziBhoi <quazibhai543@gmail.com>
2024-07-27 00:03:55 +0600

    Update doss.sh

--- doss.sh
+++ doss.sh
@@ -32,11 +32,12 @@ nohup mpirun -np 80 vasp
 echo  -e  "111\n1\n"  |  vaspkit
 
 echo  -e  "115\nBi\ns\nBi\np\nBi\nd\n"  |  vaspkit
-cp PDOS_USER.dat PDOS_Bi
-echo  -e  "115\nMo\ns\nMo\np\nMo\nd\n"  |  vaspkit
-cp PDOS_USER.dat PDOS_Mo
+cp PDOS_USER.dat Bi.dat
+echo  -e  "115\nCr\ns\nCr\np\nCr\nd\n"  |  vaspkit
+cp PDOS_USER.dat Cr.dat
 echo  -e  "115\nO\ns\nO\np\n"  |  vaspkit
-cp PDOS_USER.dat PDOS_O
+cp PDOS_USER.dat O.dat
+rm PDOS_USER.dat
 
 cp ~/bash/dosplot.py ./
 python dosplot.py
