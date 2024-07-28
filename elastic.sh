d16545462022a53323267bd8aa3188ecfae9b12e
QuaziBhoi <quazibhai543@gmail.com>
2024-07-28 22:52:11 +0600

    Update elastic.sh

--- elastic.sh
+++ elastic.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-### starting interface ###
-echo "--------------------------------------------------------------------------------"
-echo 
 
-read -p 'Number of processing core of your system: ' np
+### IMPORTANT ###
+# Edit as required
 
-mkdir elastic
-cd elastic
-cp -r ../relax/* ./
-sed -i '/LWAVE/c\LWAVE = .FALSE.' INCAR
-sed -i '/ENCUT/c\ENCUT =  620' INCAR
-sed -i '/IBRION/c\IBRION =  6' INCAR
-sed -i 's/NCORE/#NCORE/g' INCAR
+### INSTRUCTIONS ###
+# This script makes new directory "elastic" and runs elastic calculation and data extraction
+# Run this script using: bash elastic.sh
+
+echo "-----------------------------------------------------------------------------------------------"
+
+cd .. #Go to upper directory
+mkdir elastic #Make new directory 'bs'
+cd elastic #Enter directory 'bs'
+cp -r ../relax/* ./ #Copy all files from 'dos' directory into current directory
+
+sed -i '/LWAVE/c\LWAVE = .FALSE.' INCAR #Turn of wavefunction writing to save RAM
+sed -i '/ENCUT/c\ENCUT =  620' INCAR #Change ENCUT to 620
+sed -i '/IBRION/c\IBRION =  6' INCAR #Change IBRION to 6
+sed -i 's/NCORE/#NCORE/g' INCAR #Turn off NCORE tag
 
 #### Runing VASP ####
 nohup mpirun -np 80 vasp
 
-echo  -e  "203\n"  |  vaspkit  >> Elastic_Data.txt
+echo  -e  "203\n"  |  vaspkit  >> Elastic_Data.txt #Extract elastic data
