7465bec2ea1f7ca7ed21cd7f31592fff2175c18f
QuaziBhoi <quazibhai543@gmail.com>
2024-07-28 22:43:04 +0600

    Update band.sh

--- band.sh
+++ band.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-read -p 'Number of processing core of your system: ' np
+### IMPORTANT ###
+# Edit as required
 
+### INSTRUCTIONS ###
+# This script makes new directory "bs" and runs band structure calculation and data extraction
+# Run this script using: bash band.sh
+# You can edit K=Path as required by manually editing
+
+echo "-----------------------------------------------------------------------------------------------"
+
+cd ..
+mkdir bs
+cd bs
 cp -r ../dos/* ./
-cp INCAR INCAR.st
 
+cp INCAR INCAR.st
 sed -i '5 i ICHARG = 11' INCAR
 
-nohup mpirun -np $np vasp
+#### Runing VASP ####
+nohup mpirun -np 80 vasp
 
 echo  -e  "303\n"  |  vaspkit > vaspkit.txt
 cp KPOINTS KPOINTS.r
 cp KPATH.in KPOINTS
 
-
-nohup mpirun -np $np vasp
+nohup mpirun -np 80 vasp
 echo  -e  "211\n1\n"  |  vaspkit > vaspkit.txt
